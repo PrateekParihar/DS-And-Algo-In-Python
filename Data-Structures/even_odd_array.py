c09ee6b66c13c2deb6c621d31a60e08b0325f5b3
Prateek Parihar <prateek@Prateeks-MacBook-Pro.local>
2019-07-14 22:59:29 -0400

    -- 14 july commit

--- Data-Structures/even_odd_array.py
+++ Data-Structures/even_odd_array.py
@@ -1,15 +1,35 @@
-def even_odd(A):
-    next_even, next_odd = 0, len(A)-1
+from datetime import date
+class Animal:
+    sound = 'uuuuffffff'
+    
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
 
-    while(next_even < next_odd):
-        if A[next_even] % 2 == 0:
-            next_even += 1
-        else:
-            A[next_even], A[next_odd] = A[next_odd],A[next_even]
-            next_odd -= 1
+    @staticmethod
+    def from_fathers_Age(name, father_age, father_person_age_diff):
+        return Animal(name,date.today().year - father_age + father_person_age_diff)
 
+    @classmethod
+    def from_birth_year(cls,name,birth_year):
+        return cls(name,date.today().year - birth_year)
 
-q = [2,45,6,3,2,5,7,0,-1,3,0]
-even_odd(q)
-print(q)
+    def display(self):
+        print(self.name +" age is - "+str(self.age))
 
+class Man(Animal):
+    sex = 'Male'
+
+person = Man('Adam',19)
+person.display()
+
+person1 = person.from_birth_year('John',1948)
+person1.display()
+
+person2 = person.from_fathers_Age('Ben',1956, 20)
+person2.display()
+
+print(isinstance(person1,Man))
+print(isinstance(person2,Man))
+
+help(isinstance)
---
+++ play.py
@@ -0,0 +1,21 @@
+from typing import Dict, List
+# A dictionary where the keys are strings and the values are ints
+
+name_counts: Dict[str, int] = {
+    
+"Adam": 10,
+    
+"Guido": 12
+}
+
+# A list of integers
+
+numbers: List[int] = [1, 2, 3, 4, 5, 6]
+# A list that holds dicts that each hold a string key / int value
+
+list_of_dicts: List[Dict[str, int]] = [
+    {
+"key1": 1},
+    {
+"key2": 2}
+]
